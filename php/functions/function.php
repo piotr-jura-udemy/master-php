a6b8a83a0c1e090ae377374bda26ceb0a3acbb6f
Piotr Jura <jura.piotrek@gmail.com>
2024-07-17 13:22:39 +0200

    0017-optional-arguments

--- php/functions/function.php
+++ php/functions/function.php
@@ -1,7 +1,14 @@
 <?php
 
 function greet($name) {
-  return "Hello, $name!";
+  return "Hello, $name!\n";
 }
 
 echo greet("Alice");
+
+function greetWithTime($name, $time = "day") {
+  return "Good $time, $name\n";
+}
+
+echo greetWithTime("Bob");
+echo greetWithTime("Charlie", "evening");
