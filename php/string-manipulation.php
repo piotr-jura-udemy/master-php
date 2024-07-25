5e507899f53c93dead8d0edd242eb684d9db2eec
Piotr Jura <jura.piotrek@gmail.com>
2024-07-25 14:09:08 +0200

    0034-string-access-manipulation

---
+++ php/string-manipulation.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = "Hello, World!";
+// echo $str[0];
+// echo $str[-1];
+
+// echo substr($str, 0, 5);
+// echo substr($str, 5);
+
+// echo strtoupper($str);
+echo ucfirst(strtolower($str));
+
+$greeting = "Hello, " . "World";
+$greeting .= " How are you?";
+echo $greeting;
+
