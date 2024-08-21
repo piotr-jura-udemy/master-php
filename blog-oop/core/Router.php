d764f53c178539ab10499b217fceb9ad664596bc
Piotr Jura <jura.piotrek@gmail.com>
2024-08-21 21:28:45 +0200

    0302-routing

--- blog-oop/core/Router.php
+++ blog-oop/core/Router.php
@@ -3,7 +3,19 @@
 namespace Core;
 
 class Router {
-  public function __construct() {
-    echo "Works!";
+  protected array $routes = [];
+
+  public function add(string $method, string $uri, string $controller): void {
+
+  }
+
+  public function dispatch(string $uri, string $method): string {}
+
+  protected function findRoute(string $uri, string $method): ?array {}
+
+  protected function matchRoute(string $routeUri, string $requestUri): ?array {}
+
+  protected function callAction(string $controller, string $action, array $params): string {
+    
   }
 }
