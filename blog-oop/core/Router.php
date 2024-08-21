17f106d093232d4c2dd735468c35434f7300d26d
Piotr Jura <jura.piotrek@gmail.com>
2024-08-21 19:20:11 +0200

    0301-composer-autoloading

---
+++ blog-oop/core/Router.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Core;
+
+class Router {
+  public function __construct() {
+    echo "Works!";
+  }
+}
--- contact-form/includes/router.php
+++ contact-form/includes/router.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+
 const ALLOWED_METHODS = ['GET', 'POST'];
 const INDEX_URI = '';
 const INDEX_ROUTE = 'index';
