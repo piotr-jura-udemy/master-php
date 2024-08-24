93221ed7d9ef5b28f79b0a47e93d59192a879985
Piotr Jura <jura.piotrek@gmail.com>
2024-08-24 19:48:38 +0200

    0305-views

---
+++ blog-oop/core/View.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core;
+
+use RuntimeException;
+
+class View {
+  public static function render(string $template, array $data = [], string $layout = null): string {
+    $content = static::renderTemplate(
+      $template,
+      $data
+    );
+    return static::renderLayout($layout, $data, $content);
+  }
+
+  protected static function renderTemplate(string $template, array $data): string {
+    extract($data);
+    $path = dirname(__DIR__) . "/../app/Views/$template.php";
+
+    if (!file_exists($path)) {
+      throw new RuntimeException("Error: Template file not found: $path");
+    }
+
+    ob_start();
+    require $path;
+    return ob_get_clean();
+  }
+
+  protected static function renderLayout(?string $template, array $data, string $content): string {
+    if (null === $template) {
+      return $content;
+    }
+
+    extract([...$data, 'content' => $content]);
+    $path = dirname(__DIR__) . "/../app/Views/$template.php";
+
+    if (!file_exists($path)) {
+      throw new RuntimeException("Error: Layout file not found: $path");
+    }
+
+    ob_start();
+    require $path;
+    return ob_get_clean();
+  }
+}
