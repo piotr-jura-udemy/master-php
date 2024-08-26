57866fd7dddf4d6929341958b9cd1ea56614229d
Piotr Jura <jura.piotrek@gmail.com>
2024-08-26 19:18:25 +0200

    0307-di-container

---
+++ blog-oop/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'app' => [
+    'name' => 'My Blog',
+    'debug' => true
+  ],
+  'database' => [
+    'driver' => 'sqlite',
+    'path' => __DIR__ . '/database/blog.sqlite'
+  ]
+];
--- blog-oop/public/index.php
+++ blog-oop/public/index.php
@@ -1,12 +1,10 @@
 <?php
 declare (strict_types = 1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
 
 use Core\Router;
 
 $router = new Router();
-
 require_once __DIR__ . '/../routes.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
