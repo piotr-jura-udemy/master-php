d8b84a59e1e43f22cd1fe31159b660cd48eb5c6e
Piotr Jura <jura.piotrek@gmail.com>
2024-08-28 18:55:43 +0200

    0312-inserting-into-db

--- blog-oop/core/Model.php
+++ blog-oop/core/Model.php
@@ -20,7 +20,17 @@ abstract class Model {
     return $result ? static::createFromArray($result) : null;
   }
 
-  public static function create(array $data): static {}
+  public static function create(array $data): static {
+    $db = App::get('database');
+    // 1) Get the names of columns inside $data
+    $columns = implode(', ', array_keys($data));
+    // -> id, title, created_at, content
+    $placeholders = implode(', ', array_fill(0, count($data), '?'));
+    // -> ?, ?, ?, ?
+    $sql = "INSERT INTO " . static::$table . " ($columns) VALUES ($placeholders)";
+    $db->query($sql, array_values($data));
+    return static::find($db->lastInsertId());
+  }
 
   protected static function createFromArray(array $data): static {
     $model = new static();
