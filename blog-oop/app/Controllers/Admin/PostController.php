0e8fc79b1dd3259a66aaa0979747b146e6105982
Piotr Jura <jura.piotrek@gmail.com>
2024-09-11 18:57:09 +0200

    0348-admin-posts

---
+++ blog-oop/app/Controllers/Admin/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Models\Post;
+use App\Services\Auth;
+use App\Services\Authorization;
+use Core\Router;
+use Core\View;
+
+class PostController {
+  public function index() {
+    // + Pagination
+    // + Search
+    return View::render(
+      template: 'admin/posts/index',
+      layout: 'layouts/admin',
+      data: ['posts' => Post::all()]
+    );
+  }
+
+  public function create() {
+    return View::render(
+      template: 'admin/posts/create',
+      layout: 'layouts/admin'
+    );
+  }
+
+  public function store() {
+    Authorization::verify('create_post');
+    $data = [
+      'title' => $_POST['title'],
+      'content' => $_POST['content'],
+      'user_id' => Auth::user()->id,
+    ];
+    Post::create($data);
+    Router::redirect('/admin/posts');
+  }
+
+  public function edit($id) {
+    return View::render(
+      template: 'admin/posts/edit',
+      layout: 'layouts/admin',
+      data: ['post' => Post::find($id)]
+    );
+  }
+
+  public function update($id) {
+    $post = Post::find($id);
+    Authorization::verify('edit_post', $post);
+    $post->title = $_POST['title'];
+    $post->content = $_POST['content'];
+    $post->save();
+    // + static update()
+    Router::redirect('/admin/posts');
+  }
+
+  public function delete($id) {
+    // Post::findOrFail($id)
+    // + Able to do by yourself
+    $post = Post::find($id);
+    $post->delete();
+    Router::redirect('/admin/posts');
+  }
+}
